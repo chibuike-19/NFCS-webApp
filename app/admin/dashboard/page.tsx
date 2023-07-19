35c50337a2e84ec41d72c2fc943a610efdf0cc24
chibuike-19 <emmanuelobiechina8@gmail.com>
2023-07-19 16:47:45 +0100

    worked on writing into realtime db and retrieving info from db

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -2,11 +2,18 @@
 
 import ProtectedRoute from '../../component/protectedRoute'
 import { useAuth } from '@/app/context/authService'
+import {ref, set, push, onValue} from 'firebase/database'
 import { useRouter } from 'next/navigation'
+import { db } from '@/app/context/firebase'
 import { grantModerator } from '@/lib/admin-config'
+import { useRef } from 'react'
 
 const AdminDashboard = () => {
     const router = useRouter()
+    const eventRef = useRef<HTMLInputElement>(null);
+    const desc_ref = useRef<HTMLInputElement>(null);
+    const timeRef = useRef<HTMLInputElement>(null); 
+    const dateRef = useRef<HTMLInputElement>(null);
     const {user, logOut, userEmailRef} = useAuth()
 
     // Assign admin role to a user
@@ -16,6 +23,34 @@ const AdminDashboard = () => {
         // const response = await grantModerator(email, user)
     }
 
+    const handleAddEvent = (e:any) => {
+      e.preventDefault();
+      const reference = ref(db, 'upcoming_event/')
+
+      // To retrieve data of a list
+      const event_ref = onValue(reference, (snapshot) => {
+        snapshot.forEach(snap => {
+          console.log(snap.val())
+        })
+      })
+      // Add to a list of item in a particular reference
+
+      // push(reference, {
+      //   event_name: eventRef.current?.value,
+      //   event_desc: desc_ref.current?.value,
+      //   event_date: dateRef.current?.value,
+      //   event_time: timeRef.current?.value,
+      // });
+      
+      // Add a single item to a particular reference
+
+      // set(reference, {
+      //   event_name: eventRef.current?.value,
+      //   event_desc: desc_ref.current?.value,
+      //   event_date: dateRef.current?.value,
+      //   event_time: timeRef.current?.value
+      // })
+    }
       // Logs out user
       const signOut = () => {
         logOut;
@@ -29,6 +64,15 @@ const AdminDashboard = () => {
           <label htmlFor="">Enter Email</label>
           <input type="email" name="email" id=""  />
           <input type="submit" />
+        </form>
+         <form onSubmit={handleAddEvent}>
+          <label htmlFor="">Add new Post </label>
+          <input type="text" placeholder='post name' ref={eventRef}/>
+          <input type="text" name="email" id="" ref={desc_ref} placeholder='post description' />
+          <input type="text" placeholder='post date' ref={dateRef}/>
+          <input type="text" placeholder='post time' ref={timeRef} />
+          
+          <input type="submit"  />
         </form>
         <button onClick={signOut}>Log Out</button>
       </div>
--- app/component/protectedRoute.tsx
+++ app/component/protectedRoute.tsx
@@ -1,18 +1,19 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { auth } from "../context/firebase";
+import { useEffect } from "react";
 
 function ProtectedRoute(Component: any) {
-  
   return function WithProtected(props: any) {
     const router = useRouter();
     // checks for authentication of currentUser
-    if (!auth?.currentUser) {
-      // redirects user to login page if not yet authenticated
-      router.replace("/login");
-      // displays while redirecting is occuring, could be replaced with a loader component
-      return <h1>Loading</h1>;
-    }
+    useEffect(() => {
+      if (!auth?.currentUser) {
+        // redirects user to login page if not yet authenticated
+        router.replace("/login");
+        // displays while redirecting is occuring, could be replaced with a loader component
+      }
+    }, [auth]);
 
     return <Component />;
   };
