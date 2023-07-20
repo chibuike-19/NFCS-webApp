818752a0dfe5aa47b0c315d785cf17bd9a44b1a7
Abdulmojeed <mojeedayoola58@gmail.com>
2023-07-20 04:39:18 +0100

    Added reset Password Page and its functionality

--- app/context/authService.tsx
+++ app/context/authService.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { auth } from "./firebase";
 import { useContext } from "react";
@@ -12,14 +12,13 @@ import {
   signInWithPopup,
   createUserWithEmailAndPassword,
   UserCredential,
- User,
+  User,
+  sendPasswordResetEmail,
 } from "firebase/auth";
-
-
 
 type ContextProp = {
   children: React.ReactNode;
-}; 
+};
 
 type ValueProp = {
   user: User | null;
@@ -34,48 +33,49 @@ type ValueProp = {
     email: string,
     password: string
   ) => Promise<User | undefined>;
+  resetPassword: (email: string) => Promise<any>;
 };
 
-const AuthContext = React.createContext({} as ValueProp)
+const AuthContext = React.createContext({} as ValueProp);
 
+export const AuthService = ({ children }: ContextProp) => {
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+  const [setCurrentUser, cuurentUser] = useState<User | null>(null);
 
-
-export const AuthService =  ({children}: ContextProp) => {
-
-  const router = useRouter()
-    const [user, setUser] = useState<User | null>(null)
-    const [setCurrentUser, cuurentUser] = useState<User | null>(null)
-
-    useEffect(() => {
-        const unsubcribe = auth.onAuthStateChanged((user) => {
-            setUser(user)
-        })
-        return unsubcribe
-    },[])
-  const loginWithGoogle = async() => {
+  useEffect(() => {
+    const unsubcribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return unsubcribe;
+  }, []);
+  const loginWithGoogle = async () => {
     const Provider = new GoogleAuthProvider();
 
     try {
       const userCred = await signInWithPopup(auth, Provider);
       // setUser(userCred.user);
-      router.push('/user/dashboard')
-      console.log(userCred)
+      router.push("/user/dashboard");
+      console.log(userCred);
       return userCred;
     } catch (error) {
       // return error?.message
-      console.log(error)
-      return error
+      console.log(error);
+      return error;
     }
-  }
+  };
   const loginWithEmailAndPassword = async (email: string, password: string) => {
     try {
       const res = await signInWithEmailAndPassword(auth, email, password);
-     router.push("/user/dashboard")
-     console.log(res)
+      router.push("/user/dashboard");
+      console.log(res);
       return res.user;
     } catch (error) {}
-  }
-  const createNewUserWithEmailAndPassword = async (email: string, password: string) => {
+  };
+  const createNewUserWithEmailAndPassword = async (
+    email: string,
+    password: string
+  ) => {
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
       router.push("/user/dashboard");
@@ -85,12 +85,34 @@ export const AuthService =  ({children}: ContextProp) => {
   };
   const logOut = async () => {
     return signOut(auth);
-  }
+  };
 
-//ad
-  return <AuthContext.Provider value={{user, setUser, loginWithEmailAndPassword, loginWithGoogle, logOut, createNewUserWithEmailAndPassword}}>{children}</AuthContext.Provider>
+  const resetPassword = async (email: string) => {
+    try {
+      const res = await sendPasswordResetEmail(auth, email);
+      console.log('password reset successfully, please check your mail')
+      return res
+    } catch (error) {}
+  };
+
+  //ad
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        loginWithEmailAndPassword,
+        loginWithGoogle,
+        logOut,
+        createNewUserWithEmailAndPassword,
+        resetPassword,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
-    return useContext(AuthContext)
-}
+  return useContext(AuthContext);
+};
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,22 +4,21 @@ import Link from "next/link";
 import { useRef } from "react";
 
 const Login = () => {
-
   const emailref = useRef<HTMLInputElement>(null);
   const passwordref = useRef<HTMLInputElement>(null);
   const { user, loginWithGoogle, loginWithEmailAndPassword } = useAuth();
 
-    const onFormSubmit = async(e: any) => {
-      e.preventDefault()
-      try {
-        await loginWithEmailAndPassword(
-          emailref.current!.value,
-          passwordref.current!.value
-        );
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const onFormSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      await loginWithEmailAndPassword(
+        emailref.current!.value,
+        passwordref.current!.value
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
@@ -27,11 +26,12 @@ const Login = () => {
         <p>Fill Your Info to register</p>
         <input type="email" ref={emailref} />
         <input type="password" ref={passwordref} />
-        <input type="submit" onClick={onFormSubmit}/>
+        <input type="submit" onClick={onFormSubmit} />
       </form>
       <button onClick={loginWithGoogle} className="border-2 border-red-700 p-3">
         Log in with google
       </button>
+      <Link href="/forgetPassword">Forget Password</Link>
       <div>
         <p>Yet to Login? </p> <Link href="/register">Register</Link>
       </div>
