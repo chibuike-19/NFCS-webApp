d3613b096c9989587e030419251886e46dd0f506
Abdulmojeed <mojeedayoola58@gmail.com>
2023-07-24 02:28:13 +0100

    Finished implemmenting the add Profile picture functionality

--- app/context/firebase.tsx
+++ app/context/firebase.tsx
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
-import {getDatabase, ref} from 'firebase/database'
+import {getDatabase} from 'firebase/database'
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -17,4 +18,8 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const db = getDatabase(app)
 
+
+
+// Initialize storage
+export const storage = getStorage()
 
--- app/user/dashboard/page.tsx
+++ app/user/dashboard/page.tsx
@@ -2,34 +2,66 @@
 
 import ProtectedRoute from "@/app/component/protectedRoute";
 import { useAuth } from "@/app/context/authService";
-import { getAuth } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { FormEvent, useRef } from "react";
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
 
 const UserDashboard = () => {
   const { user, logOut, updateUserProfilePicture } = useAuth();
   const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
+  const [photo, setPhoto] = useState<File | null>(null);
+  const [photoURL, setPhotoUrl] = useState<string>(
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+  );
+  const [loading, setLoading] = useState<boolean>(false);
 
   const LogOut = () => {
     logOut;
     router.push("/");
   };
 
-  const uploadPhoto = async (e: FormEvent) => {
-    e.preventDefault();
-    const userPhotoUrl = inputRef.current!.value;
-    try {
-      await updateUserProfilePicture(userPhotoUrl, user);
-    } catch (error) {}
+  useEffect(() => {
+    console.log(user);
+    if (user?.photoURL) {
+      setPhotoUrl(user.photoURL);
+    }
+  }, [user, user?.photoURL]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setPhoto(e.target.files[0]);
+    }
+  };
+
+  const uploadPhoto = async (event: FormEvent) => {
+    event.preventDefault();
+    await updateUserProfilePicture(photo, user, setLoading);
+    console.log("done");
+    setPhoto(null);
   };
 
   return (
     <div>
       Hello, {user?.displayName}
       <form onSubmit={uploadPhoto}>
-        <input type="file" accept="image/*" ref={inputRef} />
-        <button type="submit">Upload Photo</button>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleChange}
+          ref={inputRef}
+        />
+        <div className="w-40 h-40 bg-red-500 rounded-full overflow-hidden mt-5">
+          {photoURL && (
+            <img src={photoURL} alt="profile" className="object-contain" />
+          )}
+        </div>
+        {loading && <p>Loading...</p>}
+        <button
+          type="submit"
+          className="bg-[#56BCFB] text-white p-3 m-5 rounded-xl"
+        >
+          Upload Photo
+        </button>
       </form>
       <button onClick={LogOut}>log Out</button>
     </div>
