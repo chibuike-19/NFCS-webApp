216a11df3e1d09026d2b55d1b4311ded9c3e926e
chibuike-19 <emmanuelobiechina8@gmail.com>
2023-09-11 18:58:06 +0100

    started working on search functionality

--- app/(DashboardCover)/members/page.tsx
+++ app/(DashboardCover)/members/page.tsx
@@ -1,33 +1,86 @@
 "use client";
 
+import { useState } from "react";
 import { useAuth } from "../../context/authService";
 import SecondProtectedRoute from "../../component/protectedRoute2";
 import defaultImage from "@/app/component/defaultImage";
 
-
 const Members = () => {
+  const [query, setQuery] = useState("");
+  const [queryBy, setQueryBy] = useState("name");
   const { members, setMembers } = useAuth();
-  SecondProtectedRoute();
- 
+  // SecondProtectedRoute();
+
+  const handleSearch = () => {
+    let searchQuery = query.toLowerCase();
+    let filteredResult;
+
+    switch (queryBy) {
+      case "department":
+        filteredResult = members.filter((member) => {
+          if (member.department) {
+            member.department.toLowerCase().includes(searchQuery);
+          }
+        });
+        break;
+      case "name":
+        filteredResult = members.filter((member) => {
+          if (member.nickname) {
+            member.nickname.toLowerCase().includes(searchQuery);
+          }
+        });
+        break;
+      case "services":
+        filteredResult = members.filter((member) => {
+          if (member.services) {
+            member.services.toLowerCase().includes(searchQuery);
+          }
+        });
+        break;
+      default:
+        filteredResult = members;
+        break;
+    }
+    console.log(query, queryBy)
+    console.log(filteredResult)
+    setMembers(filteredResult);
+  };
 
   return (
     <div className="">
-      
-        {members?.map((member, indx) => (
-          <div key={indx}>
-            <div className="w-40 h-40 bg-red-500 rounded-full overflow-hidden mt-5">
-              <img
-                src={member?.profile_url ?? defaultImage}
-                alt="profile"
-                className="object-cover h-full w-full rounded-full"
-              />
-            </div>
-            <div>
-              <p>{member.nickname}</p>
-              <p>{member.services}</p>
-            </div>
+      <div className="flex justify-end pr-4">
+        <p>sort by</p>
+        <select
+          name=""
+          id=""
+          className="mr-2"
+          onChange={(e) => setQueryBy(e.target.value)}
+        >
+          <option value="name">name</option>
+          <option value="department">department</option>
+          <option value="services">services</option>
+        </select>
+        <input type="text" value={query} onChange={(e) => {
+          setQuery(e.target.value);
+          handleSearch()
+        }}/>
+      </div>
+
+      {members?.map((member, indx) => (
+        <div key={indx}>
+          <div className="w-40 h-40 bg-red-500 rounded-full overflow-hidden mt-5">
+            <img
+              src={member?.profile_url ?? defaultImage}
+              alt="profile"
+              className="object-cover h-full w-full rounded-full"
+            />
           </div>
-        ))}
+          <div>
+            <p>{member.nickname}</p>
+            <p>{member.services}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- app/(DashboardCover)/profile/security/page.tsx
+++ app/(DashboardCover)/profile/security/page.tsx
@@ -1,14 +1,26 @@
 "use client"
 
 import Button from "@/app/component/button";
+import { useAuth } from "@/app/context/authService";
 
 
 const Security = () => {
+
+  const {handleIsReset} = useAuth()
+  
   return (
-    <div className="sm:p-6 p-0 mb-6">
+    <div className="sm:pt-12 sm:pl-6 pt-6 mb-6">
+      <div className="text-2xl font-semibold mb-4">
+        <p>Security Settings</p>
+      </div>
       <div className="flex sm:justify-between sm:flex-row sm:gap-0 gap-8 flex-col p-4 rounded-md border-2 border-[#F1F1F1]">
-        <div><p className="text-[18px]">Change Password</p><p className="text-xs text-gray-500">set a secured password for your account</p></div>
         <div>
+          <p className="text-[18px]">Change Password</p>
+          <p className="text-xs text-gray-500">
+            set a secured password for your account
+          </p>
+        </div>
+        <div onClick={handleIsReset}>
           <Button
             value="Change Password"
             extraClassnames="rounded-lg px-6 sm:px-4 py-3"
